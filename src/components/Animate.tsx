6191e2c4e05417bad6d1481c8fe991bff1661a5c
MFW-1 <am64fw5@gmail.com>
2025-12-15 12:56:35 +0500

    All done

---
+++ src/components/Animate.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Typewriter from "typewriter-effect";
+const Animate = () => {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Ignite your digital presence.",
+          "Build your dreams online.",
+          "Power-up your digital website.",
+        ],
+        autoStart: true,
+        loop: true,
+      }}
+    />
+  );
+};
+export default Animate;
